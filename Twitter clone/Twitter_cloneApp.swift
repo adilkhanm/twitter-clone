8e98ec27db3d2409362446a5b10f2e11c8278cf2
adilkhanm <adilkhan.muratov@gmail.com>
2022-05-21 13:59:25 +0600

    Initial Commit

---
+++ Twitter clone/Twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_cloneApp.swift
+//  Twitter clone
+//
+//  Created by Adilkhan Muratov on 21.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
